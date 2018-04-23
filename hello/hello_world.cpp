794e75e5c368bfad855ce0340b677d674d094a90
jin lee <sjl070707@gmail.com>
2018-04-23 10:14:32 -0400

    f;

---
+++ hello/hello_world.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+    int year = 0 ;
+    int age = 0 ;
+    string name = " ";
+    cout << "what year is your favorite?";
+    cin >> year;
+    cout << "how interesting your year is "<<year<<"!\n";
+    
+    return 0; 
+}
